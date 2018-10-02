1a617195c46394702354c5a538b02852017a35e7
fengzhizi715 <fengzhizi715@126.com>
2018-10-02 11:01:44 +0800

    add TestGuavaCache

---
+++ example/src/main/java/TestGuavaCache.java
@@ -0,0 +1,35 @@
+import com.cv4j.rxcache.RxCache;
+import com.cv4j.rxcache.domain.Record;
+import com.cv4j.rxcache.extra.memory.GuavaCacheImpl;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Created by tony on 2018/9/29.
+ */
+public class TestGuavaCache {
+
+    public static void main(String[] args) {
+
+        RxCache.config(new RxCache.Builder().memory(new GuavaCacheImpl(100)));
+
+        RxCache rxCache = RxCache.getRxCache();
+
+        User u = new User();
+        u.name = "tony";
+        u.password = "123456";
+        rxCache.save("test",u);
+
+        Observable<Record<User>> observable = rxCache.load2Observable("test", User.class);
+
+        observable.subscribe(new Consumer<Record<User>>() {
+            @Override
+            public void accept(Record<User> record) throws Exception {
+
+                User user = record.getData();
+                System.out.println(user.name);
+                System.out.println(user.password);
+            }
+        });
+    }
+}

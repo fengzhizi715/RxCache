b9b74eddb28b1b9166779900819d1739ce93a32c
fengzhizi715 <fengzhizi715@126.com>
2018-10-02 20:47:29 +0800

    add RemoteFirstStrategy

---
+++ core/src/main/java/com/safframework/rxcache/cachestrategy/impl/RemoteFirstStrategy.java
@@ -0,0 +1,81 @@
+package com.safframework.rxcache.cachestrategy.impl;
+
+import com.safframework.rxcache.RxCache;
+import com.safframework.rxcache.cachestrategy.FlowableStrategy;
+import com.safframework.rxcache.cachestrategy.MaybeStrategy;
+import com.safframework.rxcache.cachestrategy.ObservableStrategy;
+import com.safframework.rxcache.domain.Record;
+import com.safframework.rxcache.domain.Source;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import org.reactivestreams.Publisher;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by tony on 2018/10/2.
+ */
+public class RemoteFirstStrategy implements ObservableStrategy,
+        FlowableStrategy,
+        MaybeStrategy {
+
+    @Override
+    public <T> Publisher<Record<T>> execute(RxCache rxCache, String key, Flowable<T> source, Type type) {
+
+        Flowable<Record<T>> cache = rxCache.<T>load2Flowable(key, type);
+
+        Flowable<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return remote.switchIfEmpty(cache);
+    }
+
+    @Override
+    public <T> Maybe<Record<T>> execute(RxCache rxCache, String key, Maybe<T> source, Type type) {
+
+        Maybe<Record<T>> cache = rxCache.<T>load2Maybe(key, type);
+
+        Maybe<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return remote.switchIfEmpty(cache);
+    }
+
+    @Override
+    public <T> Observable<Record<T>> execute(RxCache rxCache, String key, Observable<T> source, Type type) {
+
+        Observable<Record<T>> cache = rxCache.<T>load2Observable(key, type);
+
+        Observable<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return remote.switchIfEmpty(cache);
+    }
+}

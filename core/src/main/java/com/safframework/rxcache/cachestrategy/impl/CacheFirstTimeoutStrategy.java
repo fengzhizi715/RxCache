abd4b3fffd1dbe661b2915e4fcaa3d5843c57cda
fengzhizi715 <fengzhizi715@126.com>
2018-10-02 20:57:03 +0800

    add CacheFirstTimeoutStrategy

---
+++ core/src/main/java/com/safframework/rxcache/cachestrategy/impl/CacheFirstTimeoutStrategy.java
@@ -0,0 +1,107 @@
+package com.safframework.rxcache.cachestrategy.impl;
+
+import com.safframework.rxcache.RxCache;
+import com.safframework.rxcache.cachestrategy.FlowableStrategy;
+import com.safframework.rxcache.cachestrategy.MaybeStrategy;
+import com.safframework.rxcache.cachestrategy.ObservableStrategy;
+import com.safframework.rxcache.domain.Record;
+import com.safframework.rxcache.domain.Source;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import org.reactivestreams.Publisher;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by tony on 2018/10/2.
+ */
+public class CacheFirstTimeoutStrategy implements ObservableStrategy,
+        FlowableStrategy,
+        MaybeStrategy {
+
+    private long timestamp;
+
+    public CacheFirstTimeoutStrategy(long timestamp) {
+
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public <T> Publisher<Record<T>> execute(RxCache rxCache, String key, Flowable<T> source, Type type) {
+
+        Flowable<Record<T>> cache = rxCache.<T>load2Flowable(key, type)
+                .filter(new Predicate<Record<T>>() {
+                    @Override
+                    public boolean test(Record<T> record) throws Exception {
+                        return System.currentTimeMillis() - record.getTimestamp() <= timestamp;
+                    }
+                });
+
+        Flowable<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return cache.switchIfEmpty(remote);
+    }
+
+    @Override
+    public <T> Maybe<Record<T>> execute(RxCache rxCache, String key, Maybe<T> source, Type type) {
+
+        Maybe<Record<T>> cache = rxCache.<T>load2Maybe(key, type)
+                .filter(new Predicate<Record<T>>() {
+                    @Override
+                    public boolean test(Record<T> record) throws Exception {
+                        return System.currentTimeMillis() - record.getTimestamp() <= timestamp;
+                    }
+                });
+
+        Maybe<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return cache.switchIfEmpty(remote);
+    }
+
+    @Override
+    public <T> Observable<Record<T>> execute(RxCache rxCache, String key, Observable<T> source, Type type) {
+
+        Observable<Record<T>> cache = rxCache.<T>load2Observable(key, type)
+                .filter(new Predicate<Record<T>>() {
+                    @Override
+                    public boolean test(Record<T> record) throws Exception {
+                        return System.currentTimeMillis() - record.getTimestamp() <= timestamp;
+                    }
+                });
+
+        Observable<Record<T>> remote = source
+                .map(new Function<T, Record<T>>() {
+                    @Override
+                    public Record<T> apply(@NonNull T t) throws Exception {
+
+                        rxCache.save(key, t);
+
+                        return new Record<>(Source.CLOUD, key, t);
+                    }
+                });
+
+        return cache.switchIfEmpty(remote);
+    }
+}

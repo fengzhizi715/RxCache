35853de3ac5690bee79edde0a242c72ccc65a67d
fengzhizi715 <fengzhizi715@126.com>
2018-10-02 21:42:36 +0800

    update build.gradle

--- extra/src/main/java/com/safframework/rxcache/extra/converter/FastJSONConverter.java
+++ extra/src/main/java/com/safframework/rxcache/extra/converter/FastJSONConverter.java
@@ -2,6 +2,7 @@ package com.safframework.rxcache.extra.converter;
 
 import com.alibaba.fastjson.JSON;
 import com.safframework.rxcache.persistence.disk.converter.AbstractConverter;
+import com.safframework.rxcache.persistence.disk.encrypt.Encryptor;
 
 import java.lang.reflect.Type;
 
@@ -9,6 +10,10 @@ import java.lang.reflect.Type;
  * Created by tony on 2018/9/29.
  */
 public class FastJSONConverter extends AbstractConverter {
+
+    public FastJSONConverter(Encryptor encryptor) {
+        super(encryptor);
+    }
 
     @Override
     protected <T> T fromJson(String json, Type type) {

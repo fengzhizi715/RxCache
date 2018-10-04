79c7b9c17bb1e25886200c99bc467103e31178ba
fengzhizi715 <fengzhizi715@126.com>
2018-10-04 13:21:06 +0800

    update DynamicKey

--- core/src/main/java/com/safframework/rxcache/dynamickey/DynamicKey.java
+++ core/src/main/java/com/safframework/rxcache/dynamickey/DynamicKey.java
@@ -1,9 +1,11 @@
 package com.safframework.rxcache.dynamickey;
 
+import java.lang.reflect.Method;
+
 /**
  * Created by tony on 2018/10/4.
  */
 public interface DynamicKey {
 
-    String generateCacheKey();
+    String generateCacheKey(Object target, Method method, Object... params);
 }

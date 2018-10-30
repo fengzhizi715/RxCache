910a286202d545033782c19ecd1e5b96920f1188
fengzhizi715 <fengzhizi715@126.com>
2018-10-30 17:59:07 +0800

    add proxy module

--- core/src/main/java/com/safframework/rxcache/memory/impl/AbstractMemoryImpl.java
+++ core/src/main/java/com/safframework/rxcache/memory/impl/AbstractMemoryImpl.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**

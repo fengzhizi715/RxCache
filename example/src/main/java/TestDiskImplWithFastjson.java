f2405c56dd368e55290fce5d909af956a407d58b
fengzhizi715 <fengzhizi715@126.com>
2018-11-06 13:44:05 +0800

    add test case

---
+++ example/src/main/java/TestDiskImplWithFastjson.java
@@ -0,0 +1,49 @@
+import com.safframework.rxcache.RxCache;
+import com.safframework.rxcache.converter.FastJSONConverter;
+import com.safframework.rxcache.domain.Record;
+import com.safframework.rxcache.persistence.disk.impl.DiskImpl;
+import domain.User;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+import java.io.File;
+
+/**
+ * Created by tony on 2018/11/6.
+ */
+public class TestDiskImplWithFastjson {
+
+    public static void main(String[] args) {
+
+        File cacheDirectory = new File("aaa");
+
+        if (!cacheDirectory.exists()) {
+
+            cacheDirectory.mkdir();
+        }
+
+        DiskImpl diskImpl = new DiskImpl(cacheDirectory,new FastJSONConverter());
+
+        RxCache.config(new RxCache.Builder().persistence(diskImpl));
+
+        RxCache rxCache = RxCache.getRxCache();
+
+        User u = new User();
+        u.name = "tony";
+        u.password = "123456";
+        rxCache.save("test",u);
+
+        Observable<Record<User>> observable = rxCache.load2Observable("test", User.class);
+
+        observable.subscribe(new Consumer<Record<User>>() {
+
+            @Override
+            public void accept(Record<User> record) throws Exception {
+
+                User user = record.getData();
+                System.out.println(user.name);
+                System.out.println(user.password);
+            }
+        });
+    }
+}

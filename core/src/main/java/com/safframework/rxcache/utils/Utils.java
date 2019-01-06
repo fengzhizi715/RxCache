af3e3360ed8baf57c50961402e5eb19623e10763
fengzhizi715 <fengzhizi715@126.com>
2019-01-06 12:14:13 +0800

    update build.gradle

--- core/src/main/java/com/safframework/rxcache/utils/Utils.java
+++ core/src/main/java/com/safframework/rxcache/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.safframework.rxcache.utils;
 
+import com.safframework.bytekit.Bytes;
 import com.safframework.bytekit.bytes.ByteBufferBytes;
 
 /**
@@ -23,14 +24,7 @@ public class Utils {
      * @return
      */
     public static byte[] parseHexStr2Byte(String hexStr) {
-        if (hexStr.length() < 1)
-            return null;
-        byte[] result = new byte[hexStr.length()/2];
-        for (int i = 0;i< hexStr.length()/2; i++) {
-            int high = Integer.parseInt(hexStr.substring(i*2, i*2+1), 16);
-            int low = Integer.parseInt(hexStr.substring(i*2+1, i*2+2), 16);
-            result[i] = (byte) (high * 16 + low);
-        }
-        return result;
+
+        return Bytes.getByteArray(hexStr);
     }
 }

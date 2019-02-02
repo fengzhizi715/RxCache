97faed1518dbbbe14065edc9d90029b7f1a33dc3
fengzhizi715 <fengzhizi715@126.com>
2019-02-03 00:24:04 +0800

    完善注释

--- core/src/main/java/com/safframework/rxcache/config/Constant.java
+++ core/src/main/java/com/safframework/rxcache/config/Constant.java
@@ -5,11 +5,13 @@ package com.safframework.rxcache.config;
  */
 public class Constant {
 
+    /** record ttl 相关的常量 */
     public static final long NO_RECORD = -2;
 
     public static final long NEVER_EXPIRE = -1;
 
     public static final long HAS_EXPIRED = 0;
 
-    public static final int DEFAULT_CACHE_SIZE = 512;
+
+    public static final int DEFAULT_CACHE_SIZE = 512; // 默认的缓存个数
 }

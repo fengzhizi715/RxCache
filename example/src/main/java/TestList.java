8653f3f5c0f0543902ea1a32b546cf0bca4a82fe
fengzhizi715 <fengzhizi715@126.com>
2019-02-03 10:59:55 +0800

    add TestList

---
+++ example/src/main/java/TestList.java
@@ -0,0 +1,55 @@
+import com.safframework.bytekit.utils.Preconditions;
+import com.safframework.rxcache.RxCache;
+import com.safframework.rxcache.domain.Record;
+import com.safframework.rxcache.persistence.disk.impl.DiskImpl;
+import domain.User;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tony on 2019-02-03.
+ */
+public class TestList {
+
+    public static void main(String[] args) {
+
+        RxCache.config(new RxCache.Builder());
+
+        RxCache rxCache = RxCache.getRxCache();
+
+        List<User> list = new ArrayList<>();
+
+        User u1 = new User();
+        u1.name = "tony1";
+        u1.password = "123456";
+        list.add(u1);
+
+        User u2 = new User();
+        u2.name = "tony2";
+        u2.password = "123456";
+        list.add(u2);
+        rxCache.save("test",list);
+
+        Observable<Record<List<User>>> observable = rxCache.load2Observable("test", User.class);
+
+        observable.subscribe(new Consumer<Record<List<User>>>() {
+
+            @Override
+            public void accept(Record<List<User>> record) throws Exception {
+
+                List<User> recordDataList = record.getData();
+
+                if (Preconditions.isNotBlank(recordDataList)) {
+
+                    User user = recordDataList.get(0);
+                    System.out.println(user.name);
+                    System.out.println(user.password);
+                }
+            }
+        });
+    }
+}

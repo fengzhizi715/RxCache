326fa2a853122bd3a8e64473911071bfdf9cd51d
fengzhizi715 <fengzhizi715@126.com>
2019-02-08 11:42:37 +0800

    修改注释

--- core/src/main/java/com/safframework/rxcache/domain/CacheStrategy.java
+++ core/src/main/java/com/safframework/rxcache/domain/CacheStrategy.java
@@ -1,7 +1,7 @@
 package com.safframework.rxcache.domain;
 
 /**
- * 缓存存放的策略，缓存支持存放到内存、持久层获取两者都存放。
+ * 缓存存放的策略，缓存的对象支持存放到内存、持久层或者两者都存放该对象。
  * Created by tony on 2018-12-28.
  */
 public enum CacheStrategy {

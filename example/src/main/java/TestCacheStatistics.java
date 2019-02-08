29060da682697b9553c0b2325597ab3ccacd0fc1
fengzhizi715 <fengzhizi715@126.com>
2019-02-08 13:10:42 +0800

    add TestCacheStatistics

---
+++ example/src/main/java/TestCacheStatistics.java
@@ -0,0 +1,24 @@
+import com.safframework.rxcache.RxCache;
+import com.safframework.rxcache.memory.impl.FIFOMemoryImpl;
+
+/**
+ * Created by tony on 2019-02-08.
+ */
+public class TestCacheStatistics {
+
+    public static void main(String[] args) {
+
+        RxCache.config(new RxCache.Builder().memory(new FIFOMemoryImpl(10)));
+
+        RxCache rxCache = RxCache.getRxCache();
+
+        for (int i=0;i<10;i++) {
+
+            rxCache.save("test"+i,i);
+        }
+
+        rxCache.save("test10",10);
+
+        System.out.println(rxCache.info());
+    }
+}

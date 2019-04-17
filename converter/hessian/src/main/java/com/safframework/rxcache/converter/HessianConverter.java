dec8cf74c222db6b725bf10ea20602693bfe9023
fengzhizi715 <fengzhizi715@126.com>
2019-04-17 19:33:08 +0800

    update HessianUtils

--- converter/hessian/src/main/java/com/safframework/rxcache/converter/HessianConverter.java
+++ converter/hessian/src/main/java/com/safframework/rxcache/converter/HessianConverter.java
@@ -4,7 +4,6 @@ import com.safframework.bytekit.Bytes;
 import com.safframework.bytekit.bytes.ByteBufferBytes;
 import com.safframework.rxcache.persistence.converter.AbstractConverter;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 
@@ -16,24 +15,12 @@ public class HessianConverter extends AbstractConverter {
     @Override
     public <T> T fromJson(String json, Type type) {
 
-        try {
-            return HessianUtils.deserialize(Bytes.parseBase64(json));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return HessianUtils.deserialize(Bytes.parseBase64(json));
     }
 
     @Override
     public String toJson(Object data) {
 
-        try {
-            return new String(ByteBufferBytes.create(HessianUtils.serialize(data)).encodeBase64(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return new String(ByteBufferBytes.create(HessianUtils.serialize(data)).encodeBase64(), StandardCharsets.UTF_8);
     }
 }
--- converter/hessian/src/main/java/com/safframework/rxcache/converter/HessianUtils.java
+++ converter/hessian/src/main/java/com/safframework/rxcache/converter/HessianUtils.java
@@ -2,6 +2,7 @@ package com.safframework.rxcache.converter;
 
 import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
+import com.safframework.bytekit.utils.IOUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -12,29 +13,48 @@ import java.io.IOException;
  */
 public class HessianUtils {
 
-    public static byte[] serialize(Object msg) throws IOException {
+    public static byte[] serialize(Object msg) {
         Hessian2Output out = null;
+        ByteArrayOutputStream bos = null;
         try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bos = new ByteArrayOutputStream();
             out = new Hessian2Output(bos);
             out.writeObject(msg);
             out.flush();
             return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         } finally {
+            IOUtils.closeQuietly(bos);
             if(out!=null){
-                out.close();
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    public static <T> T deserialize(byte[] buf) throws IOException {
+    public static <T> T deserialize(byte[] buf) {
         Hessian2Input input = null;
+        ByteArrayInputStream bis = null;
         try {
-            input = new Hessian2Input(new ByteArrayInputStream(buf));
+            bis = new ByteArrayInputStream(buf);
+            input = new Hessian2Input(bis);
             return (T) input.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         } finally {
+            IOUtils.closeQuietly(bis);
             if(input!=null){
-                input.close();
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }

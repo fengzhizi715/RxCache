8125ded81d36e51cd7556d37e870faf9ddb3e120
fengzhizi715 <fengzhizi715@126.com>
2019-04-20 22:38:47 +0800

    优化代码

--- core/src/main/java/com/safframework/rxcache/RxCache.java
+++ core/src/main/java/com/safframework/rxcache/RxCache.java
@@ -5,8 +5,26 @@ import com.safframework.rxcache.domain.Record;
 import com.safframework.rxcache.memory.Memory;
 import com.safframework.rxcache.memory.impl.FIFOMemoryImpl;
 import com.safframework.rxcache.persistence.Persistence;
-import com.safframework.rxcache.transformstrategy.*;
-import io.reactivex.*;
+import com.safframework.rxcache.transformstrategy.CompletableStrategy;
+import com.safframework.rxcache.transformstrategy.FlowableStrategy;
+import com.safframework.rxcache.transformstrategy.MaybeStrategy;
+import com.safframework.rxcache.transformstrategy.ObservableStrategy;
+import com.safframework.rxcache.transformstrategy.SingleStrategy;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableSource;
+import io.reactivex.CompletableTransformer;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableTransformer;
+import io.reactivex.Maybe;
+import io.reactivex.MaybeSource;
+import io.reactivex.MaybeTransformer;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.SingleTransformer;
 import org.reactivestreams.Publisher;
 
 import java.io.PrintStream;

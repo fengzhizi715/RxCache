531fe3a83fa83e9771990b441065f34d6dc74b70
fengzhizi715 <fengzhizi715@126.com>
2020-02-10 00:20:36 +0800

    update RxCache+Extension.kt

--- extension/src/main/kotlin/com/safframework/rxcache/ext/RxCache+Extension.kt
+++ extension/src/main/kotlin/com/safframework/rxcache/ext/RxCache+Extension.kt
@@ -18,7 +18,7 @@ inline fun <reified T> RxCache.transformObservable(key: String, strategy: Observ
 
 inline fun <reified T> RxCache.transformFlowable(key: String, strategy: FlowableStrategy): FlowableTransformer<T, Record<T>> = transformFlowable<T>(key, object : TypeToken<T>() {}.type, strategy)
 
-inline fun <reified T> RxCache.transformFlowable(key: String, strategy: FlowableStrategy,backpressureStrategy: BackpressureStrategy): FlowableTransformer<T, Record<T>> = transformFlowable<T>(key, object : TypeToken<T>() {}.type, strategy,backpressureStrategy)
+inline fun <reified T> RxCache.transformFlowable(key: String, strategy: FlowableStrategy,backpressureStrategy: BackpressureStrategy): FlowableTransformer<T, Record<T>> = transformFlowable<T>(key, object : TypeToken<T>() {}.type, strategy, backpressureStrategy)
 
 inline fun <reified T> RxCache.transformSingle(key: String, strategy: SingleStrategy): SingleTransformer<T, Record<T>> = transformSingle<T>(key, object : TypeToken<T>() {}.type, strategy)
 
@@ -30,6 +30,8 @@ inline fun <reified T> RxCache.transformMaybe(key: String, strategy: MaybeStrate
 inline fun <reified T> RxCache.load2Observable(key: String): Observable<Record<T>> = load2Observable<T>(key, object : TypeToken<T>() {}.type)
 
 inline fun <reified T> RxCache.load2Flowable(key: String): Flowable<Record<T>> = load2Flowable<T>(key, object : TypeToken<T>() {}.type)
+
+inline fun <reified T> RxCache.load2Flowable(key: String,backpressureStrategy: BackpressureStrategy): Flowable<Record<T>> = load2Flowable<T>(key, object : TypeToken<T>() {}.type, backpressureStrategy)
 
 inline fun <reified T> RxCache.load2Single(key: String): Single<Record<T>> = load2Single<T>(key, object : TypeToken<T>() {}.type)
 

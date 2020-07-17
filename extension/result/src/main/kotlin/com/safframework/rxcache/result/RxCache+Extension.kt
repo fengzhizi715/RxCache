08b46d9375d9dd0a5cf8f3f1ab49827e118e570d
fengzhizi715 <fengzhizi715@126.com>
2020-07-17 13:05:16 +0800

    add RxCache+Extension.kt

---
+++ extension/result/src/main/kotlin/com/safframework/rxcache/result/RxCache+Extension.kt
@@ -0,0 +1,18 @@
+package com.safframework.rxcache.result
+
+import cn.netdiscovery.result.Result
+import cn.netdiscovery.result.resultFrom
+import com.safframework.rxcache.RxCache
+import com.safframework.rxcache.reflect.TypeToken
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.rxcache.ext.`RxCache+Extension`
+ * @author: Tony Shen
+ * @date: 2020-07-17 11:10
+ * @version: V1.0 <描述当前版本功能>
+ */
+inline fun <reified T> RxCache.getResult(key: String): Result<T,Exception> = resultFrom {
+    get<T>(key, object : TypeToken<T>() {}.type).data
+}

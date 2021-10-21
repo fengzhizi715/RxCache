503ba9a221e14a2093ffb74f0964de03a5e35564
Tony Shen <fengzhizi715@126.com>
2021-10-21 17:06:20 +0800

    update

--- memory/ohc/src/main/java/com/safframework/rxcache/memory/StringSerializer.java
+++ memory/ohc/src/main/java/com/safframework/rxcache/memory/StringSerializer.java
@@ -13,56 +13,47 @@ import java.nio.charset.Charset;
  */
 public class StringSerializer implements CacheSerializer<String> {
 
-    /**
-     * 计算字符串序列化后占用的空间
-     *
-     * @param value 需要序列化存储的字符串
-     * @return 序列化后的字节数
-     */
+    private static Charset UTF_8 = Charset.forName("UTF-8");
+
     @Override
-    public int serializedSize(String value) {
-        byte[] bytes = value.getBytes(Charset.forName("UTF-8"));
-
-        // 设置字符串长度限制，2^16 = 65536
-        if (bytes.length > 65536)
-            throw new RuntimeException("encoded string too long: " + bytes.length + " bytes");
-
-        // 设置字符串长度限制，2^16 = 65536
-        return bytes.length + 2;
+    public void serialize(String s, ByteBuffer byteBuffer) {
+        byte[] bytes = s.getBytes(UTF_8);
+        byteBuffer.put((byte) ((bytes.length >>> 8) & 0xFF));
+        byteBuffer.put((byte) ((bytes.length >>> 0) & 0xFF));
+        byteBuffer.put(bytes);
     }
 
-
-    /**
-     * 将字符串对象序列化到 ByteBuffer 中，ByteBuffer是OHC管理的堆外内存区域的映射。
-     *
-     * @param value 需要序列化的对象
-     * @param buf   序列化后的存储空间
-     */
     @Override
-    public void serialize(String value, ByteBuffer buf) {
-        // 得到字符串对象UTF-8编码的字节数组
-        byte[] bytes = value.getBytes(Charset.forName("UTF-8"));
-        // 用前16位记录数组长度
-        buf.put((byte) ((bytes.length >>> 8) & 0xFF));
-        buf.put((byte) ((bytes.length) & 0xFF));
-        buf.put(bytes);
-    }
-
-
-    /**
-     * 对堆外缓存的字符串进行反序列化
-     *
-     * @param buf 字节数组所在的 ByteBuffer
-     * @return 字符串对象.
-     */
-    @Override
-    public String deserialize(ByteBuffer buf) {
-        // 判断字节数组的长度
-        int length = (((buf.get() & 0xff) << 8) + ((buf.get() & 0xff)));
+    public String deserialize(ByteBuffer byteBuffer) {
+        int length = (((byteBuffer.get() & 0xff) << 8) + ((byteBuffer.get() & 0xff) << 0));
         byte[] bytes = new byte[length];
-        // 读取字节数组
-        buf.get(bytes);
-        // 返回字符串对象
-        return new String(bytes, Charset.forName("UTF-8"));
+        byteBuffer.get(bytes);
+        return new String(bytes, UTF_8);
+    }
+
+    @Override
+    public int serializedSize(String s) {
+        return writeUTFLen(s);
+    }
+
+    static int writeUTFLen(String str) {
+        int strlen = str.length();
+        int utflen = 0;
+        int c;
+
+        for (int i = 0; i < strlen; i++) {
+            c = str.charAt(i);
+            if ((c >= 0x0001) && (c <= 0x007F))
+                utflen++;
+            else if (c > 0x07FF)
+                utflen += 3;
+            else
+                utflen += 2;
+        }
+
+        if (utflen > 65535)
+            throw new RuntimeException("encoded string too long: " + utflen + " bytes");
+
+        return utflen + 2;
     }
 }

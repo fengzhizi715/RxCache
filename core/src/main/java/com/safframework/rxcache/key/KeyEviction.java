5cdee9322bb0ff8fda10ce5632dd844bda6553c5
Tony Shen <fengzhizi715@126.com>
2023-02-13 16:14:10 +0800

    Update KeyEviction.java

--- core/src/main/java/com/safframework/rxcache/key/KeyEviction.java
+++ core/src/main/java/com/safframework/rxcache/key/KeyEviction.java
@@ -4,9 +4,9 @@ package com.safframework.rxcache.key;
  * @FileName: com.safframework.rxcache.key.KeyEviction
  * @author: Tony Shen
  * @date: 2021-02-09 21:14
- * @version: V1.0 <描述当前版本功能>
+ * @version: V1.0 key 淘汰的枚举，包含同步淘汰 key 和 异步淘汰 key
  */
 public enum KeyEviction {
-    SYNC,
-    ASYNC
+    SYNC, // 默认为 同步淘汰 key的方式，在获取缓存 key 的同时，会判断是否过期
+    ASYNC // 异步淘汰 key 的方式: RxCache 内部定时判断所有的 key ，如果有过期的 key，则删除
 }

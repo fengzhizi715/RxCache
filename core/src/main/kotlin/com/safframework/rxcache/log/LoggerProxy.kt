ae967f13b38e40be8219187e23fe6ed08b7e1a77
Tony Shen <fengzhizi715@126.com>
2023-02-14 13:53:49 +0800

    refactor

--- core/src/main/kotlin/com/safframework/rxcache/log/LoggerProxy.kt
+++ core/src/main/kotlin/com/safframework/rxcache/log/LoggerProxy.kt
@@ -8,6 +8,14 @@ package com.safframework.rxcache.log
  * @date: 2023/2/13 8:00 PM
  * @version: V1.0 <描述当前版本功能>
  */
+interface Logger {
+    fun i(msg: String, tag: String? = "rxcache")
+    fun v(msg: String, tag: String? = "rxcache")
+    fun d(msg: String, tag: String? = "rxcache")
+    fun w(msg: String, tag: String? = "rxcache", tr: Throwable?)
+    fun e(msg: String, tag: String? = "rxcache", tr: Throwable?)
+}
+
 internal object LoggerProxy {
 
     private lateinit var mLogger: Logger
